dd0bb5dd25b27ccbee10634b1671b3ca4732f2d8
Flor2801 <florenciasarria1@hotmail.com>
2020-11-23 19:16:56 -0300

    Modal bienvenida

---
+++ index.js
@@ -0,0 +1,9 @@
+const overlay = document.getElementById('bienvenida')
+const modalBienvenida = document.getElementById('modal-inicial')
+const botonBienvenida = document.getElementById('boton-bienvenida')
+
+
+botonBienvenida.onclick = () => {
+    modalBienvenida.classList.add('hidden')
+
+}
