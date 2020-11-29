6cba04ef10757b579b93b28990afd31d68f0fcdf
Eugenia Nunez <eugeiq@gmail.com>
2020-11-29 20:27:02 -0300

    agregue seleccionar items y funcionalidad timer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const overlay = document.getElementById('bienvenida')
 const modalBienvenida = document.getElementById('modal-inicial')
 const botonBienvenida = document.getElementById('boton-bienvenida')
+const modalFinDeJuego = document.getElementById('fin-de-juego')
+const parrafoSegundos = document.getElementById("segundos")
 
 
 const grilla = document.querySelector(".grilla");
@@ -11,79 +13,225 @@ const nuevoJuego = document.getElementById("nuevo-juego");
 // const reiniciarJuego = document.getElementById("reiniciar-juego");
 
 
-
-
 // Grilla
 
-let items = ["🍉", "🍐", "🍌", "🍇", "🍎", "🍊", "🍑", "🥥", "🍒", "🍋"];
-let listaDeFrutas = [];
-let frutas = "";
+let items = ["🐷", "🐹", "🦊", "🐶", "🐴", "🐔", "🐷", "🐸", "🐤", "🐱"];
+let listaDeAnimales = [];
+let animales = "";
 
 const obtenerNumeroAlAzar = (items) => {
   let largo = items.length;
   return Math.floor(Math.random() * largo);
 };
-const obtenerFrutaAlAzar = (items) => {
+const obtenerAnimalAlAzar = (items) => {
   return items[obtenerNumeroAlAzar(items)];
 };
 
 const crearGrilla = (ancho, alto) => {
-  const anchoDeGrilla = 50 * ancho;
-  grilla.style.width = `${anchoDeGrilla}px`;
 
   for (let i = 0; i < ancho; i++) {
-    listaDeFrutas[i] = [];
+    listaDeAnimales[i] = [];
     for (let j = 0; j < alto; j++) {
-      listaDeFrutas[i][j] = obtenerFrutaAlAzar(items);
+      listaDeAnimales[i][j] = obtenerAnimalAlAzar(items);
     }
   }
-
-  grilla.innerHTML = "";
-  for (let i = 0; i < listaDeFrutas.length; i++) {
-    for (let j = 0; j < listaDeFrutas[i].length; j++) {
-      frutas = obtenerFrutaAlAzar(items);
-      listaDeFrutas[i][j] = frutas;
-      grilla.innerHTML += `<div data-x="${i}" data-y="${j}"> ${frutas}</div>`;
-    }
-  }
-
-  return grilla;
+  return listaDeAnimales
 };
 
-nuevoJuego.onclick = () => {
-    crearGrilla(6, 6);
+
+const generarCelda = (x, y, array) => {
+  const tamanio = 50
+
+  const celda = document.createElement('div')
+  celda.dataset.x = x
+  celda.dataset.y = y
+  celda.innerHTML = array[x][y]
+  celda.style.top = `${x * tamanio}px`
+  celda.style.left = `${y * tamanio}px`
+  celda.addEventListener('click', seleccionarItem)
+  return celda
+
 }
 
-botonBienvenida.onclick = () => {
-    modalBienvenida.classList.add('hidden')
-    overlay.classList.add('hidden')
-    crearGrilla(6, 6);
+const agregarGrillaAHTML = (ancho) => {
+  const anchoDeGrilla = 50 * ancho
+  grilla.style.width = `${anchoDeGrilla}px`
+  grilla.innerHTML = ""
+  for (let i = 0; i < listaDeAnimales.length; i++) {
+    for (let j = 0; j < listaDeAnimales[i].length; j++) {
+      grilla.appendChild(generarCelda(i, j, listaDeAnimales))
+    }
+  }
 }
+
+const iniciarJuego = () => {
+  crearGrilla(6, 6)
+  agregarGrillaAHTML(6)
+
+  let limiteDeTiempo = new Date()
+  limiteDeTiempo.setSeconds(limiteDeTiempo.getSeconds()+30)
+
+  comenzarCuentaRegresiva(limiteDeTiempo)
+}
+
+nuevoJuego.onclick = () => {
+  iniciarJuego()
+  modalFinDeJuego.classList.add("hidden")
+  overlay.classList.add('hidden')
+
+}
+
+
+botonBienvenida.onclick = () => {
+  modalBienvenida.classList.add('hidden')
+  overlay.classList.add('hidden')
+  iniciarJuego();
+ 
+}
+
+const mostrarModalFinDeJuego = () => {
+  modalFinDeJuego.classList.remove("hidden")
+  overlay.classList.remove('hidden')
+
+}
+//Timer
+
+const obtenerTiempoFaltante = (limiteDeTiempo) => {
+  let fechaActual = new Date()
+  let tiempoFaltante = ((limiteDeTiempo - fechaActual) + 1000) / 1000
+  let segundosFaltantes = Math.floor(tiempoFaltante)
+  if (segundosFaltantes.toString().length === 1) {
+    return "0" + segundosFaltantes
+  }
+  else {
+    return segundosFaltantes
+  }
+
+}
+
+const comenzarCuentaRegresiva=(limiteDeTiempo)=>{
+//setInterval ejecuta una función (esta funcion obtiene el tiempo restante en cada 
+//momento y lo inserta en el p) o un fragmento de código de forma repetitiva 
+//cada vez que termina el periodo de tiempo determinado (1000 milisegundos).
+  const actualizadorDeTiempo = setInterval(()=>{
+
+    let segundos=obtenerTiempoFaltante(limiteDeTiempo)
+    parrafoSegundos.textContent= "0 : " + segundos
+    
+
+    if(segundos==="00"){
+      //clearInterval cancela una acción reiterativa que se inició mediante una llamada a setInterval.
+      //(actualizadorDeTiempo) es el identificador de la acción reiterativa que se desea cancelar.
+      clearInterval(actualizadorDeTiempo)
+      mostrarModalFinDeJuego()
+    }
+
+  },1000)
+
+}
+
+
+const seleccionarItem = (e) => {
+  let primeraCeldaSeleccionada = document.querySelector(".remarcar")
+  //Si ya existe una celda seleccionada
+  if (primeraCeldaSeleccionada != null) {
+    if (sonAdyacentes(primeraCeldaSeleccionada, e.target)) {
+      intercambiarCeldas(primeraCeldaSeleccionada, e.target)
+    }
+    else {
+      primeraCeldaSeleccionada.classList.remove("remarcar")
+      e.target.classList.add("remarcar")
+    }
+
+  }
+  else (
+    e.target.classList.add("remarcar")
+  )
+}
+//Devuelve verdadero si dos celdas son adyacentes y falso si no lo
+
+const sonAdyacentes = (celda1, celda2) => {
+  const datax1 = Number(celda1.dataset.x)
+  const datax2 = Number(celda2.dataset.x)
+  const datay1 = Number(celda1.dataset.y)
+  const datay2 = Number(celda2.dataset.y)
+  if ((datax1 === datax2 && datay1 === datay2 + 1)
+    || (datax1 === datax2 && datay1 === datay2 - 1)
+    || (datay1 === datay2 && datax1 === datax2 + 1)
+    || (datay1 === datay2 && datax1 === datax2 - 1)) {
+    return true
+  }
+  else {
+    return false
+  }
+
+}
+
+const intercambiarCeldas = (celda1, celda2) => {
+  const datax1 = Number(celda1.dataset.x)
+  const datax2 = Number(celda2.dataset.x)
+  const datay1 = Number(celda1.dataset.y)
+  const datay2 = Number(celda2.dataset.y)
+
+  const tamanio = 50
+
+  // aqui modifico la grilla en JS
+  let variableTemporal = listaDeAnimales[datax1][datay1]
+  listaDeAnimales[datax1][datay1] = listaDeAnimales[datax2][datay2]
+  listaDeAnimales[datax2][datay2] = variableTemporal
+
+  // aqui modifico la grilla en HTML
+
+  if (datax1 === datax2 && (datay1 === datay2 + 1 || datay1 === datay2 - 1)) {
+    celda1.style.left = `${datay2 * tamanio}px`
+    celda2.style.left = `${datay1 * tamanio}px`
+    celda1.dataset.y = datay2
+    celda2.dataset.y = datay1
+
+  }
+
+  else if (datay1 === datay2 && (datax1 === datax2 + 1 || datax1 === datax2 - 1)) {
+    celda1.style.top = `${datax2 * tamanio}px`
+    celda2.style.top = `${datax1 * tamanio}px`
+    celda1.dataset.x = datax2
+    celda2.dataset.x = datax1
+
+  }
+
+}
+
+
+
+
+
+
+
+
 
 // const ocultarBotones = () => {
 //     botonFacil.classList.add("ocultar");
 //     botonMedio.classList.add("ocultar");
 //     botonDificil.classList.add("ocultar");
 // };
-  
+
 // botonFacil.onclick = () => {
 //     crearGrilla(6, 6);
 //     ocultarBotones();
 //     reiniciarJuego.classList.add("facil");
 // };
-  
+
 // botonMedio.onclick = () => {
 //     crearGrilla(8, 8);
 //     ocultarBotones();
 //     reiniciarJuego.classList.add("medio");
 // };
-  
+
 // botonDificil.onclick = () => {
 //     crearGrilla(10, 10);
 //     ocultarBotones();
 //     reiniciarJuego.classList.add("dificil");
 // };
-  
+
 // nuevoJuego.onclick = () => {
 //     botonFacil.classList.toggle("ocultar");
 //     botonMedio.classList.toggle("ocultar");
@@ -92,7 +240,7 @@ botonBienvenida.onclick = () => {
 //     reiniciarJuego.classList.remove("medio");
 //     reiniciarJuego.classList.remove("dificil");
 // };
-  
+
 // reiniciarJuego.onclick = () => {
 //     if (reiniciarJuego.classList.contains("facil")) {
 //       crearGrilla(6, 6);
@@ -102,4 +250,5 @@ botonBienvenida.onclick = () => {
 //       crearGrilla(10, 10);
 //     }
 // };
-  
+
+
